6ba2ca5b399316c6ec1ecf6ee8ae0aeeb70d17f7
afiatamanna06 <afiatamanna06@gmail.com>
2019-12-04 21:11:19 +0600

    I.py updated

---
+++ PythonPro/I.py
@@ -0,0 +1,13 @@
+x = 10
+if x > 10:
+    print("Greater")
+elif x < 10:
+    print("Smaller")
+else:
+    print("Equal to 10")
+if 10 > 9 or 1 == 2:
+    print("Or performed")
+if 10 > 9 and 10 > 1:
+    print("And performed")
+if 200 > 33:
+    pass
