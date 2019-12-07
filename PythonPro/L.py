b4fcad9c70cbadb4896f76c20c960e7e8c0810b5
afiatamanna06 <afiatamanna06@gmail.com>
2019-12-07 09:48:54 +0600

    L.py updated

---
+++ PythonPro/L.py
@@ -0,0 +1,36 @@
+def myfunction(name="Afia"):
+    print("Welcome", name)
+
+
+myfunction()
+myfunction("Ahana")
+
+
+def thisfunction(food):
+    for x in food:
+        print(x)
+
+
+fruits = ["apple", "banana", "mango"]
+
+thisfunction(fruits)
+
+
+def multiplication(x):
+    return 5 * x
+
+
+print(multiplication(3))
+print(multiplication(5))
+print(multiplication(7))
+
+
+def thirdchild(child1, child2, child3):
+    print("Third child is",child3)
+
+
+thirdchild(child1="Robert", child2="Pattrich", child3="Delilah")
+
+
+def func():
+    pass
