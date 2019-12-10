f6512ac748801e814f53f85d7978e5f4ab282da3
afiatamanna06 <afiatamanna06@gmail.com>
2019-12-10 12:35:51 +0600

    Q.py updated

--- PythonPro/Q.py
+++ PythonPro/Q.py
@@ -11,3 +11,28 @@ print(next(it))
 print("In for loop:")
 for x in mystr:
     print(x)
+
+
+class MyClass:
+    def __iter__(self):
+        self.a = 1
+        return self
+
+    def __next__(self):
+        if self.a <= 20:
+            x = self.a
+            self.a += 1
+            return x
+        else:
+            raise StopIteration
+
+
+mycls = MyClass()
+myitra = iter(mycls)
+
+print(next(myitra))
+print(next(myitra))
+print(next(myitra))
+
+for x in myitra:
+    print(x)
