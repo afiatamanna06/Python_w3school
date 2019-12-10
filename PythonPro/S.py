6f748c6f8fbdaf141982d03513cdb649330594d7
afiatamanna06 <afiatamanna06@gmail.com>
2019-12-10 13:54:47 +0600

    S.py updated

---
+++ PythonPro/S.py
@@ -0,0 +1,9 @@
+import mymodule as mx
+mx.greetings("Afia")
+a = mx.person["age"]
+print(a)
+
+import platform
+w = platform.system()
+print(w)
+print(dir(platform))
---
+++ PythonPro/mymodule.py
@@ -0,0 +1,9 @@
+def greetings(name):
+    print("Hello "+name+"!")
+
+
+person = {
+    "name": "Ahana",
+    "age": 11,
+    "class": 7
+}
