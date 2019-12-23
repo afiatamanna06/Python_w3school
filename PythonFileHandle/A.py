d999d2d08cc385d7011c50fa2a43db84d2a6a831
afiatamanna06 <afiatamanna06@gmail.com>
2019-12-23 12:40:44 +0600

    A.py updated

--- PythonFileHandle/A.py
+++ PythonFileHandle/A.py
@@ -1,8 +1,25 @@
 f = open("demo.txt", "a")
 f.write("This is a demo file.")
-f.write("I am writing it.")
+f.write("I am writing it.\n")
+f.write("I'm just wonder if you love me.\n")
+f.close()
+
+
+f = open("demo.txt", "r")
+print(f.read(5))
 f.close()
 
 
 f = open("demo.txt", "r")
 print(f.read())
+f.close()
+
+
+f = open("demo.txt", "r")
+print(f.readline())
+f.close()
+
+
+f = open("demo.txt", "r")
+for x in f:
+    print(x)
